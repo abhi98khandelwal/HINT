df4af9aa7125b1b8cc6c417d211b9e5b00490a1f
Rajat Gupta <rajat15101@iiitnr.edu.in>
2017-03-24 18:54:29 +0530

    Added data handling function for NEO Engine

--- neo.py
+++ neo.py
@@ -1,45 +1,72 @@
-from bs4 import BeautifulSoup
-import urllib.parse
-import urllib.request
-import urllib.error
+import datetime
+import random
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
+import get_online_data
 
-"""
-base_url = 'http://runningstatus.in/history'
-train_no = str(input("Enter Train No."))
-querr = {'querry':train_no}
-querr = urllib.parse.urlencode(querr)
-url = '/'.join([base_url,querr])
-response = urllib.request.urlopen(url)
-html_bytes = response.read()
-html= html_bytes.decode('utf-8')
-soup = BeautifulSoup(html,'html.parser')
 
 class Neo_Engine:
 
     def __int__(self):
-        self.clf = LinearRegression()
+        self.clf = KNeighborsClassifier()
+        #elf.regressor = LinearRegression()
 
-    def getdata(self,df):
+    def getdata_and_train(self,train_no):
+        get_online_data.Create_DataFile(train_no)
+        df = pd.read_csv('data.csv')
+        df.fillna(value=0, inplace=True)
+        new_df = df.apply(LabelEncoder().fit_transform)
+        X = new_df.drop(['Status'], axis=1)
+        X = X.drop(['ReachTime'], axis=1)
+        y = new_df['Status']
+        self.clf.fit(X, y)
+
         # Train the model of particular locomotive
         # Example : Get train data from user. convert results to DataFrame and feed it.
-        pass
 
     def predict_delay(self):
+        X = preprocess_and_add_data()
+        prediction = self.clf.predict(X)
+        print(prediction)
         #based on training will predict delay of locomotive next day
-"""
 
 
-df = pd.read_csv('data.csv')
-df.fillna(value=0,inplace=True)
-new_df = df.drop(['Status'],axis=1)
-#new_df = new_df.drop(['ReachTime'],axis=1)
 
-new_df = new_df.apply(LabelEncoder().fit_transform)
 
-X = new_df.drop(['Delay'],axis=1)
-y = new_df['Delay']
+def preprocess_and_add_data():
+    df = pd.read_csv('data.csv')
+    df.fillna(value=0, inplace=True)
+    columns = df.columns.values
+    index = 0
+    for column in columns[1:2]:
+        X = df[column]
+        for i, x in enumerate(X):
+            if x != 'Late' and x != 'Before':
+                index = i
+                break
+    X = None
+    for column in columns[:1]:
+        X = df[column][index]
+        break
+    new_time = str(datetime.datetime.now())
+    X = list(X)
+    new_time = list(new_time)
+    print(X)
+    print(new_time)
+    X[:11] = new_time[:11]
+    X = "".join(X)
+    data_dict = {
+        'StartedOn':[X],
+        'Delay':df['Delay'][random.randint(0,len(df))]
+    }
+    enc = LabelEncoder()
+    new_df = pd.DataFrame(data=data_dict)
+    new_df = new_df.apply((LabelEncoder().fit_transform))
+    return new_df
 
-print(new_df)
+
+
+
+
